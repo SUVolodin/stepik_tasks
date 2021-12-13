874e7265112612149c39e890964a793d74fd7b2b
SUVolodin <volodin.stas87@gmail.com>
2021-12-13 22:34:17 +0300

    Простой калькулятор.

---
+++ first_week/sixth_task_first_week.py
@@ -0,0 +1,24 @@
+"""
+Простой калькулятор, принимающий на ввод три строки:
+число 1, число 2 и операцию.
+Поддерживаемые операции: +, -, /, *, mod, pow, div.
+"""
+a = float(input())
+b = float(input())
+c = input()
+if c == '+':
+    print(a + b)
+if c == '-':
+    print(a - b)
+if c == '*':
+    print(a * b)
+if c == 'pow':
+    print(a ** b)
+if (c == '/' or c == 'div' or c == 'mod') and b == 0:
+    print('Деление на 0!')
+elif c == '/':
+    print(a / b)
+elif c == 'div':
+    print(a // b)
+elif c == 'mod':
+    print(a % b)
