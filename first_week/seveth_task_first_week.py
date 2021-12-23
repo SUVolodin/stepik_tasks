422a3d2ddea28440aaa5d1b402ac09445f0a9a67
SUVolodin <volodin.stas87@gmail.com>
2021-12-23 19:20:28 +0300

    fix

--- first_week/seveth_task_first_week.py
+++ first_week/seveth_task_first_week.py
@@ -4,18 +4,18 @@
 PI = 3.14
 form = input()
 if form == 'треугольник':
-    a = float(input())
-    b = float(input())
-    c = float(input())
+    a = float(input('Введите длинну первой стороны треугольника: '))
+    b = float(input('Введите длинну второй стороны треугольника :'))
+    c = float(input('Введите длинну третьей стороны треугольника :'))
     p = (a + b + c) / 2
     x = (p - a)
     y = (p - b)
     z = (p - c)
     print(((p * x * y * z)**0.5))
 if form == 'прямоугольник':
-    a = float(input())
-    b = float(input())
+    a = float(input('Введите длинну прямоугольника :'))
+    b = float(input('Введите ширину прямоугольника :'))
     print(a * b)
 if form == 'круг':
-    r = float(input())
+    r = float(input('Введите радиус окружности :'))
     print(PI * (r ** 2))
